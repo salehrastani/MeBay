1bb0483bd5211d417ad2a77c42bb23e3293ff37a
Saleh Rastani <salehrastani@gmail.com>
2015-06-28 12:23:31 -0700

    add user_id to products migration; add users as devise model; authenticate_user! in application controller; add new products; add new user

--- db/migrate/20150628182633_create_products.rb
+++ db/migrate/20150628182633_create_products.rb
@@ -3,6 +3,7 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.string :image
+      t.integer :user_id
 
       t.timestamps null: false
     end
